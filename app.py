f84dd84bace345a38d9bddad6772eff2671747b9
Douglas Victor Cedrum <douglsa.viced@gmail.com>
2020-04-01 16:20:31 +0200

    Updated app.py and added app.run

--- app.py
+++ app.py
@@ -78,3 +78,8 @@ def delete_recipe(recipe_id):
     else:
         res = make_response(jsonify({"message": "Invalid Password"}), 401)
     return res
+
+if __name__ == "__main__":
+        app.run(host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
